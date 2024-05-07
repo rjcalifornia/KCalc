f03dd9a00c89c97f8fec2fa8d493ea29989664d8
rjcalifornia <rodolfohernan20@gmail.com>
2024-05-07 10:01:30 -0600

    Initial commit 	Changes to be committed: 	new file:   .gitignore 	new file:   .metadata 	new file:   README.md

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:ubuntu_calculator/ui/screens/home.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey.shade900,
+      ),
+    );
+  }
+}
