7fa4f94ce10104965608d85cf34e3573b75062a6
rjcalifornia <rodolfohernan20@gmail.com>
2024-05-20 07:43:12 -0600

    Changes to be committed: 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ubuntu_calculator/ui/screens/home.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowManager.instance.setMinimumSize(const Size(480, 720));
+  WindowManager.instance.setMaximumSize(const Size(480, 720));
+
   runApp(const HomeScreen());
 }
