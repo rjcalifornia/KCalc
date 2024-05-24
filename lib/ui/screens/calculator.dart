4c90278b37662184a5e44eeb208adfa1893ba47d
rjcalifornia <rodolfohernan20@gmail.com>
2024-05-24 07:57:10 -0600

    Changes to be committed: 	modified:   lib/ui/screens/calculator.dart 	modified:   lib/ui/widgets/button_area.dart

--- lib/ui/screens/calculator.dart
+++ lib/ui/screens/calculator.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ubuntu_calculator/ui/components/button_helper.dart';
+
 import 'package:ubuntu_calculator/ui/widgets/switch.dart';
 
 class CalculatorScreen extends StatefulWidget {
@@ -27,7 +27,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         elevation: 0,
         backgroundColor: const Color(0xff2c2c37),
         leading: const Icon(
-          Icons.settings_outlined,
+          Icons.menu_outlined,
           color: Color(0xfffafafa),
         ),
       ),
@@ -38,160 +38,6 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             const SizedBox(
               height: 8,
             ),
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
-                      child: Wrap(
-                        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        alignment: WrapAlignment.spaceAround,
-                        spacing: 18,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buttonHelper(
-                                  'AC',
-                                  const Color(0xff23252d),
-                                  const Color(0xff02c984),
-                                  () => buttonPressed('AC')),
-                              buttonHelper(
-                                  '+/-',
-                                  const Color(0xff23252d),
-                                  const Color(0xff02c984),
-                                  () => buttonPressed('+/-')),
-                              buttonHelper(
-                                  '%',
-                                  const Color(0xff23252d),
-                                  const Color(0xff02c984),
-                                  () => buttonPressed('%')),
-                              buttonHelper(
-                                  "÷",
-                                  const Color(0xff23252d),
-                                  const Color(0xffc14a51),
-                                  () => buttonPressed('÷')),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buttonHelper(
-                                  '7',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('7')),
-                              buttonHelper(
-                                  '8',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('8')),
-                              buttonHelper(
-                                  '9',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('9')),
-                              buttonHelper(
-                                  "*",
-                                  const Color(0xff23252d),
-                                  const Color(0xffc14a51),
-                                  () => buttonPressed('*')),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buttonHelper(
-                                  '4',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('4')),
-                              buttonHelper(
-                                  '5',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('5')),
-                              buttonHelper(
-                                  '6',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('6')),
-                              buttonHelper(
-                                  "-",
-                                  const Color(0xff23252d),
-                                  const Color(0xffc14a51),
-                                  () => buttonPressed('-')),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buttonHelper(
-                                  '1',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('4')),
-                              buttonHelper(
-                                  '2',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('5')),
-                              buttonHelper(
-                                  '3',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('6')),
-                              buttonHelper(
-                                  "+",
-                                  const Color(0xff23252d),
-                                  const Color(0xffc14a51),
-                                  () => buttonPressed('+')),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 18,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              const SizedBox(
-                                width: 75,
-                              ),
-                              buttonHelper(
-                                  '0',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('0')),
-                              buttonHelper(
-                                  '.',
-                                  const Color(0xff23252d),
-                                  const Color(0xffffffff),
-                                  () => buttonPressed('.')),
-                              buttonHelper(
-                                  "=",
-                                  const Color(0xff23252d),
-                                  const Color(0xffc14a51),
-                                  () => buttonPressed('=')),
-                            ],
-                          )
-                        ],
-                      ),
-                    ))
-              ],
-            )),
             const SizedBox(
               height: 8,
             ),
--- lib/ui/widgets/button_area.dart
+++ lib/ui/widgets/button_area.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ubuntu_calculator/ui/components/button_helper.dart';
 
 class ButtonArea extends StatefulWidget {
   const ButtonArea({super.key});
@@ -10,6 +11,102 @@ class ButtonArea extends StatefulWidget {
 class _ButtonAreaState extends State<ButtonArea> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0),
+              child: Wrap(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                alignment: WrapAlignment.spaceAround,
+                spacing: 18,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttonHelper('AC', const Color(0xff23252d),
+                          const Color(0xff02c984), () => buttonPressed('AC')),
+                      buttonHelper('+/-', const Color(0xff23252d),
+                          const Color(0xff02c984), () => buttonPressed('+/-')),
+                      buttonHelper('%', const Color(0xff23252d),
+                          const Color(0xff02c984), () => buttonPressed('%')),
+                      buttonHelper("÷", const Color(0xff23252d),
+                          const Color(0xffc14a51), () => buttonPressed('÷')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttonHelper('7', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('7')),
+                      buttonHelper('8', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('8')),
+                      buttonHelper('9', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('9')),
+                      buttonHelper("*", const Color(0xff23252d),
+                          const Color(0xffc14a51), () => buttonPressed('*')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttonHelper('4', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('4')),
+                      buttonHelper('5', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('5')),
+                      buttonHelper('6', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('6')),
+                      buttonHelper("-", const Color(0xff23252d),
+                          const Color(0xffc14a51), () => buttonPressed('-')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      buttonHelper('1', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('4')),
+                      buttonHelper('2', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('5')),
+                      buttonHelper('3', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('6')),
+                      buttonHelper("+", const Color(0xff23252d),
+                          const Color(0xffc14a51), () => buttonPressed('+')),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const SizedBox(
+                        width: 75,
+                      ),
+                      buttonHelper('0', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('0')),
+                      buttonHelper('.', const Color(0xff23252d),
+                          const Color(0xffffffff), () => buttonPressed('.')),
+                      buttonHelper("=", const Color(0xff23252d),
+                          const Color(0xffc14a51), () => buttonPressed('=')),
+                    ],
+                  )
+                ],
+              ),
+            ))
+      ],
+    ));
   }
 }
